077197a12a0976eeabfdac386751031a9fa32887
hanxue111 <1360230174@qq.com>
2019-04-19 22:15:07 +0800

    第一次修改

---
+++ js/huawei.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+	//黑色导航中集团网站的隐藏部分
+	$(".website").click(function() {
+		$(".websiteShow").slideToggle();
+	});
+
+	//黑色导航中选择区域/语言的隐藏部分	
+	$(".language").click(function() {
+		$(".languageShow").slideToggle();
+	})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
